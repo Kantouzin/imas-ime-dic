c65d1a9af5f4e9ce20fa5ff2e61f37a3b98d0d1c
Kantouzin <kantouzin0113@gmail.com>
2020-04-21 12:54:28 +0900

    Fix to use data for replacement / addition

--- imascraping/imascraping.py
+++ imascraping/imascraping.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import json
+import pickle
 
 import regex
 from urllib import request, parse
@@ -35,9 +37,14 @@ class FullName:
         r"\p{Katakana}")
 
     def __init__(self, name):
-        (self.last, self.first,
-         self.kana_last, self.kana_first,
-         self.is_fgn) = self._init_name(name)
+        if type(name) is str:
+            (self.last, self.first,
+             self.kana_last, self.kana_first,
+             self.is_fgn) = self._init_name(name)
+        elif type(name) is dict:
+            self.last, self.first = name["last"], name["first"]
+            self.kana_last, self.kana_first = name["kana_last"], name["kana_first"]
+            self.is_fgn = name["is_fgn"] if "is_fgn" in name else False
 
     def _init_name(self, name):
         ja_match = FullName.ja_pt.search(name)
@@ -70,6 +77,12 @@ class FullName:
     def is_all_kana_first(self):
         return FullName.all_kana_pt.search(self.first)
 
+    @classmethod
+    def is_name(cls, text):
+        ja_match = FullName.ja_pt.search(text)
+        fgn_match = FullName.fgn_pt.match(text)
+        return ja_match or fgn_match
+
     @staticmethod
     def _kata_to_hira(text):
         return "".join(
@@ -82,90 +95,163 @@ class FullName:
             "(" + self.kana_last + " " + self.kana_first + ")")
 
 
-def formatting(li):
-    return "".join(list(map(lambda text: text + "\r\n", li)))
+class DicGenerator:
+    def __init__(self, url_ja, begin_id, end_id,
+                 dic_name="dic", tag_get="dt"):
+        self.url_ja = url_ja
+        self.begin_id, self.end_id = begin_id, end_id
+        self.dic_name = dic_name
+        self.tag_get = tag_get
 
+    def make_dic(self):
+        name_list = self.get_name_list()
 
-def make_dic(url_ja, begin_id, end_id, dic_name="dic.txt", tag_get="dt"):
-    url = parse.quote_plus(url_ja, "/:?=&")
-    html = request.urlopen(url)
-    soup = BeautifulSoup(html, "html.parser")
+        full_list = list()
+        last_list = list()
+        first_list = list()
 
-    begin_tag = soup.find(id=begin_id).parent
-    end_tag = soup.find(id=end_id).parent
-    tag_list = begin_tag.find_next_siblings()
-    tag_list = tag_list[:tag_list.index(end_tag)]
-    tag_list = BeautifulSoup(
-        "".join([str(tag) for tag in tag_list]), "html.parser"
-    ).find_all(tag_get)
+        for name in name_list:
+            full_list.append(
+                name.get_kana() + "\t" + name.get_name() + "\t" + "人名")
 
-    full_list = list()
-    last_list = list()
-    first_list = list()
-
-    for tag in tag_list:
-        try:
-            n = FullName(tag.text)
-
-            full_list.append(n.get_kana() + "\t" + n.get_name() + "\t" + "人名")
-
-            if n.is_fgn:
+            if name.is_fgn:
                 continue
 
-            if not n.is_all_kana_last():
-                last_list.append(n.kana_last + "\t" + n.last + "\t" + "姓")
+            if not name.is_all_kana_last():
+                last_list.append(
+                    name.kana_last + "\t" + name.last + "\t" + "姓")
 
-            if not n.is_all_kana_first():
-                first_list.append(n.kana_first + "\t" + n.first + "\t" + "名")
-        except NameError:
-            continue
+            if not name.is_all_kana_first():
+                first_list.append(
+                    name.kana_first + "\t" + name.first + "\t" + "名")
 
-    full_list = list(dict.fromkeys(full_list))
-    last_list = list(dict.fromkeys(last_list))
-    first_list = list(dict.fromkeys(first_list))
+        full_list = list(dict.fromkeys(full_list))
+        last_list = list(dict.fromkeys(last_list))
+        first_list = list(dict.fromkeys(first_list))
 
-    path = Path("dic/")
-    if not path.exists():
-        path.mkdir()
-    path /= Path(dic_name + ".txt")
+        path = Path("dic/")
+        if not path.exists():
+            path.mkdir()
+        path /= Path(self.dic_name + ".txt")
 
-    with path.open("w", encoding="utf-8", newline="\r\n") as file:
-        file.write(formatting(full_list))
-        file.write(formatting(last_list))
-        file.write(formatting(first_list))
+        with path.open("w", encoding="utf-8", newline="\r\n") as file:
+            file.write(self.formatting(full_list))
+            file.write(self.formatting(last_list))
+            file.write(self.formatting(first_list))
 
-    print(str(path) + " ->")
-    print("人名: " + str(len(full_list)))
-    print("姓　: " + str(len(last_list)))
-    print("名　: " + str(len(first_list)))
-    print()
+        print(
+            str(path) + " ->" + "\n" +
+            "人名: " + str(len(full_list)) + "\n" +
+            "姓　: " + str(len(last_list)) + "\n" +
+            "名　: " + str(len(first_list)) + "\n"
+        )
+
+    def get_raw_name_list(self):
+        dump_path = Path("dump/" + self.dic_name + ".pkl")
+
+        if not Path("dump/").exists():
+            Path("dump").mkdir()
+
+        if dump_path.exists():
+            with dump_path.open("rb") as f:
+                name_list = pickle.load(f)
+            print("Load from dump")
+        else:
+            url = parse.quote_plus(self.url_ja, "/:?=&")
+            html = request.urlopen(url)
+            soup = BeautifulSoup(html, "html.parser")
+
+            begin_tag = soup.find(id=self.begin_id).parent
+            end_tag = soup.find(id=self.end_id).parent
+            tag_list = begin_tag.find_next_siblings()
+            tag_list = tag_list[:tag_list.index(end_tag)]
+            tag_list = BeautifulSoup(
+                "".join([str(tag) for tag in tag_list]), "html.parser"
+            ).find_all(self.tag_get)
+
+            text_list = [
+                tag.text for tag in tag_list if FullName.is_name(tag.text)
+            ]
+
+            name_list = [FullName(text) for text in text_list]
+
+            with dump_path.open("wb") as f:
+                pickle.dump(name_list, f)
+            print("Load from Wikipedia")
+
+        return name_list
+
+    def get_name_list(self, replace_json=None, add_json=None):
+        raw_name_list = self.get_raw_name_list()
+        name_list = self.replace_name_list(raw_name_list)
+        name_list = self.add_name_list(name_list)
+        return name_list
+
+    def replace_name_list(self, name_list):
+        with Path("json/replace.json").open("r") as f:
+            replace_dic = json.load(f)
+
+        new_name_list = name_list
+        for before, after in replace_dic.items():
+            new_name_list = [name
+                             if name.get_name() != before
+                             else FullName(after)
+                             for name in new_name_list]
+
+        return new_name_list
+
+    def add_name_list(self, name_list):
+        with Path("json/add.json").open("r") as f:
+            add_dic = json.load(f)
+
+        new_name_list = name_list
+        for source, target_list in add_dic.items():
+            for i, name in enumerate(new_name_list):
+                if name.get_name() == source:
+                    index = i + 1
+                    break
+            else:
+                index = -1
+            if index < 0:
+                continue
+            for target in target_list:
+                new_name_list.insert(index, FullName(target))
+                index += 1
+
+        return new_name_list
+
+    @staticmethod
+    def formatting(li):
+        return "".join(list(map(lambda text: text + "\r\n", li)))
 
 
 def main():
-    make_dic(
-        "https://ja.wikipedia.org/wiki/THE_IDOLM@STERの登場人物",
-        "765（ナムコ）プロダクション所属アイドル", "765プロダクション社員",
-        dic_name="765pro", tag_get="h3")
+    dic_generator_list = [
+        DicGenerator(
+            "https://ja.wikipedia.org/wiki/THE_IDOLM@STERの登場人物",
+            "765（ナムコ）プロダクション所属アイドル", "765プロダクション社員",
+            dic_name="765pro", tag_get="h3"),
+        DicGenerator(
+            "https://ja.wikipedia.org/wiki/アイドルマスター_シンデレラガールズ",
+            "登場キャラクター",
+            "他シリーズ出身のアイドル達",
+            dic_name="cinderella"),
+        DicGenerator(
+            "https://ja.wikipedia.org/wiki/アイドルマスター_ミリオンライブ!の登場人物",
+            "765THEATER_ALLSTARS", "765PRO_ALLSTARS",
+            dic_name="millionlive"),
+        DicGenerator(
+            "https://ja.wikipedia.org/wiki/アイドルマスター_SideM",
+            "登場キャラクター", "その他の登場人物",
+            dic_name="sidem"),
+        DicGenerator(
+            "https://ja.wikipedia.org/wiki/アイドルマスター_シャイニーカラーズ",
+            "登場人物", "CD",
+            dic_name="shinycolors")
+    ]
 
-    make_dic(
-        "https://ja.wikipedia.org/wiki/アイドルマスター_シンデレラガールズ",
-        "登場キャラクター", "他プロダクション",
-        dic_name="cinderella")
-
-    make_dic(
-        "https://ja.wikipedia.org/wiki/アイドルマスター_ミリオンライブ!の登場人物",
-        "765THEATER_ALLSTARS", "765PRO_ALLSTARS",
-        dic_name="millionlive")
-
-    make_dic(
-        "https://ja.wikipedia.org/wiki/アイドルマスター_SideM",
-        "登場キャラクター", "その他の登場人物",
-        dic_name="sidem")
-
-    make_dic(
-        "https://ja.wikipedia.org/wiki/アイドルマスター_シャイニーカラーズ",
-        "登場人物", "CD",
-        dic_name="shinycolors")
+    for dic_generator in dic_generator_list:
+        dic_generator.make_dic()
 
 
 if __name__ == "__main__":
